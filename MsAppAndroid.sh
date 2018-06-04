b949705d4aa5b132d88ffa635b9d82b5b17810ac
lmacheru <hlogzaogza@gmail.com>
2018-06-04 13:31:44 +0200

    changes

--- MsAppAndroid.sh
+++ MsAppAndroid.sh
@@ -5,20 +5,21 @@
 
 ### You will have to update these variables for your environment
 export CURRENTDATE=`date +"%Y-%m-%d %I.%M.%S %p"`
-export PRJLOC=".\Mobile"
+export PRJLOC="full path of project"
 export ANDROID_DEVICE_ID="04c460ef"
-export TEST_ASSEMBLIES="${PRJLOC}\VodaiOSPRODLocal\VodaiOS\bin\Debug"
-export APK="C:\Users\bbdnet1454\Desktop\Projects\XTC\za.co.vodacom.android.app_Version 9.9.0 (790).apk"
+export TEST_ASSEMBLIES="${PRJLOC}\MobileApps\MobileApps\bin\Debug"
+export APK="*fullpath*\za.co.vodacom.android.app_Version 9.9.0 (790).apk"
 export SERIES="master"
 export APPNAME="hlogzaogza/Android_VSP"
 export LOCALE=en_US
-export TESTDATA1="${PRJLOC}\VodaiOSPRODLocal\VodaiOS\bin\Debug\Envtestdata"
-export TESTDATA2="${PRJLOC}\VodaiOSPRODLocal\VodaiOS\bin\Debug\EnvtestOR"
+export TESTDATA1="${PRJLOC}\MobileApps\MobileApps\bin\Debug\Envtestdata"
+export TESTDATA2="${PRJLOC}\MobileApps\MobileApps\bin\Debug\EnvtestOR"
 export XMLRESULTLOC="${APPNAME}\XTCresults[${CURRENTDATE}].xml"
-export Logloc="C:\Users\bbdnet1454\Desktop\Projects\XTC\Log\[${CURRENTDATE}].log"
+export Logloc=".\Log\[${CURRENTDATE}].log"
 
 
 appcenter test run uitest --app "${APPNAME}" --devices "${ANDROID_DEVICE_ID}" --app-path "${APK}" --test-series "${SERIES}" --locale "${LOCALE}" --build-dir "${TEST_ASSEMBLIES}" --include "${TESTDATA1}" --include "${TESTDATA2}" 
 
 
 
+///MobileApps/bin/Debug/
--- MsAppIos.sh
+++ MsAppIos.sh
@@ -5,19 +5,17 @@
 
 ### You will have to update these variables for your environment
 export CURRENTDATE=`date +"%Y-%m-%d %I.%M.%S %p"`
-export PRJLOC="C:\Users\bbdnet1454\Desktop\Projects\XTC\Mobile"
+export PRJLOC="full path of project"
 export IOS_DEVICE_ID="hlogzaogza/ios"
-export TEST_ASSEMBLIES="${PRJLOC}\VodaiOSPRODLocal\VodaiOS\bin\Debug"
-export APK="C:\Users\bbdnet1454\Desktop\Projects\XTC\MyVodacom_Hockey_9.9.ipa"
+export TEST_ASSEMBLIES="${PRJLOC}\MobileApps\MobileApps\bin\Debug"
+export APK="*fullpath*\MyVodacom_Hockey_9.9.ipa"
 export SERIES="master"
 export APPNAME="hlogzaogza/VSP_IOS"
 export LOCALE=en_US
-export TESTDATA1="${PRJLOC}\VodaiOSPRODLocal\VodaiOS\bin\Debug\Envtestdata"
-export TESTDATA2="${PRJLOC}\VodaiOSPRODLocal\VodaiOS\bin\Debug\EnvtestOR"
+export TESTDATA1="${PRJLOC}\MobileApps\MobileApps\bin\Debug\Envtestdata"
+export TESTDATA2="${PRJLOC}\MobileApps\MobileApps\bin\Debug\EnvtestOR"
 export XMLRESULTLOC="${APPNAME}\XTCresults[${CURRENTDATE}].xml"
-export Logloc="C:\Users\bbdnet1454\Desktop\Projects\X
-TC\Log\[${CURRENTDATE}].log"
-
+export Logloc="*Full path*\Log\[${CURRENTDATE}].log"
 
 appcenter test run uitest --app "${APPNAME}" --devices "${IOS_DEVICE_ID}" --app-path "${APK}" --test-series "${SERIES}" --locale "${LOCALE}" --build-dir "${TEST_ASSEMBLIES}" --include "${TESTDATA1}" --include "${TESTDATA2}" 
 
